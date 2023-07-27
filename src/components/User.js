8b6ed54e27490cf3e4f3ef8cd288350f3c6e413d
Suraj Mahapatra <surajjiku@gmail.com>
2023-07-28 00:59:08 +0530

    Done

--- src/components/User.js
+++ src/components/User.js
@@ -4,10 +4,20 @@ import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
 import SearchIcon from '@mui/icons-material/Search';
 import { Avatar } from '@mui/material'
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useDispatch} from 'react-redux';
+import { logout } from '../features/userSlice';
+import { auth } from './firebase';
 
 function User() {
 
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const logoutOfApp = () =>{
+    dispatch(logout())
+    auth.signOut();
+  };
 
   return (
     <div className='header'> 
@@ -25,6 +35,12 @@ function User() {
             <Avatar className='headerOption_icon' src={user?.photoURL}></Avatar>
             <h6 className='headerOption_title'>{user?.displayName}</h6>
         </div>
+
+        <div onClick={logoutOfApp}>
+          <LogoutIcon sx={{fontSize:25}}/>
+        </div>
+
+        
     
     </div>
   )
